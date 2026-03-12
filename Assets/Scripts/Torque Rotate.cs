6df6f8c45edfff55c8fbf7b7b12290aac4ac018f
KetoGuitarz <thanawut428@gmail.com>
2026-03-12 11:05:10 +0700

    all check

--- Assets/Scripts/Torque Rotate.cs
+++ Assets/Scripts/Torque Rotate.cs
@@ -3,20 +3,20 @@ using UnityEngine.InputSystem;
 
 public class TorqueRotate : MonoBehaviour
 {
-    public float torqueSpeed;
+    public float TorqueRotateSpeed;
     Rigidbody rb;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
     void Start()
     {
-        rb.GetComponent<Rigidbody>();
+        rb = GetComponent<Rigidbody>();
     }
-
-    // Update is called once per frame
-    void FixedUpdate()
+    private void FixedUpdate()
     {
         if (Keyboard.current.dKey.isPressed)
         {
-            rb.AddTorque(new Vector3(torqueSpeed, 0, 0));
+            rb.AddTorque(new Vector3(0, 0, TorqueRotateSpeed));
         }
+
+
     }
 }
